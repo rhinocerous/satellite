d91105c35263e77de1453ba816776ce03155f50c
autodealio <rhinocerous+autodealio@gmail.com>
2015-07-10 18:15:52 -0700

    refactor main layout

--- api/controllers/IndexController.js
+++ api/controllers/IndexController.js
@@ -4,6 +4,6 @@ module.exports =
     res.view("homepage", {}); //  default index from config/views.js is used since we don't specify a layout
   },
   alternate: function (req, res) {
-    res.view("homepage", {layout: 'layout_alternate'}); //  now we specify an alternate layout
+    res.view("homepage", {layout: 'layout'}); //  now we specify an alternate layout
   }
 };
