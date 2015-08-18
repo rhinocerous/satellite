35c7cd259e0e637c26fea2abbb3ac587db07f86a
autodealio <rhinocerous+autodealio@gmail.com>
2015-08-17 21:17:39 -0700

    working on uploader

---
+++ assets/js/ng/angular-dropzone.js
@@ -0,0 +1,30 @@
+
+/**
+ * An AngularJS directive for Dropzone.js, http://www.dropzonejs.com/
+ *
+ * Usage:
+ *
+ * <div ng-app="app" ng-controller="SomeCtrl">
+ *   <button dropzone="dropzoneConfig">
+ *     Drag and drop files here or click to upload
+ *   </button>
+ * </div>
+ */
+
+
+
+//angular.module('app', ['dropzone']);
+//
+//angular.module('app').controller('SomeCtrl', function ($scope) {
+//  $scope.dropzoneConfig = {
+//    'options': { // passed into the Dropzone constructor
+//      'url': 'upload.php'
+//    },
+//    'eventHandlers': {
+//      'sending': function (file, xhr, formData) {
+//      },
+//      'success': function (file, response) {
+//      }
+//    }
+//  };
+//});
