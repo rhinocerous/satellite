595b2c3ac9f7c3a7e47f9c56a443516efe863f35
autodealio <rhinocerous+autodealio@gmail.com>
2015-08-19 21:55:31 -0700

    fix scroll to top

--- assets/js/prestige/custom.js
+++ assets/js/prestige/custom.js
@@ -19,6 +19,14 @@ function init() {
   onePageScroll();
   scrollAnchor();
   toggleContactForm();
+
+//  http://stackoverflow.com/a/22419676
+  if ( ($(window).height() + 100) < $(document).height() ) {
+    $('#top-link-block').removeClass('hidden').affix({
+      // how far to scroll down before link "slides" into view
+      offset: {top:100}
+    });
+  }
 }
 
 
